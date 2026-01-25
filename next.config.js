2cae74520b2185e714f25f546b23393cec4ab605
unknown <saidragragui7@gmail.com>
2026-01-25 02:32:41 -0500

    Add Cloudinary to allowed image domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'leefii.com', 'cdn.midjourney.com'],
+    domains: ['images.unsplash.com', 'leefii.com', 'cdn.midjourney.com', 'res.cloudinary.com'],
     unoptimized: true
   },
   async headers() {
